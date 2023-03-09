34deb2c9ada7a4bb089f9a23a9a8b4f180fc2005
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-03-09 21:24:53 +0300

    Edited mypy config, small refactoring

--- mysite/models.py
+++ mysite/models.py
@@ -5,13 +5,13 @@ from django.core.validators import MinValueValidator
 
 
 class Position(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.SlugField()
-    description = models.TextField()
-    image = models.ImageField()
-    price = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
-    is_with_discount = models.BooleanField(default=False)
-    default_discount = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
+    title: models.CharField = models.CharField(max_length=200)
+    url: models.SlugField = models.SlugField()
+    description: models.TextField = models.TextField()
+    image: models.ImageField = models.ImageField()
+    price: models.FloatField = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
+    is_with_discount: models.BooleanField = models.BooleanField(default=False)
+    default_discount: models.FloatField = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
 
     def __str__(self):
         return self.title
