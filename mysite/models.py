60762623318fe973a7f3bb3319409b46ff6a667d
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-03-16 15:00:03 +0300

    Index template created, Position model created

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Position(models.Model):
+    title: models.CharField = models.CharField(max_length=200)
+    url: models.SlugField = models.SlugField()
+    description: models.TextField = models.TextField()
+    image: models.ImageField = models.ImageField()
+    price: models.FloatField = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
+    is_with_discount: models.BooleanField = models.BooleanField(default=False)
+    default_discount: models.FloatField = models.FloatField(validators=[MinValueValidator(Decimal('0.01'))])
+
+    def __str__(self):
+        return self.title
