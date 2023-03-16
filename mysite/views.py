fc94d566425fa313e7a04e44b426684505d74de5
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-03-16 20:11:18 +0300

    MainView added

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,18 @@
+from .models import Position
+
 from django.shortcuts import render
 from django.views import View
 
 
 class MainView(View):
+    """Александр"""
     def get(self, request, *args, **kwargs):
+        positions = Position.objects.all()
         return render(
             request,
-            'mysite/index.html'
+            'mysite/index.html',
+            context={
+                'positions': positions,
+                'nav_bar': 'index'
+            }
         )
