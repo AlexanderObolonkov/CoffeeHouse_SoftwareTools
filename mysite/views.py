e90e81d1670d49091edeed4d978ef414a806cbf9
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-04-04 11:26:15 +0300

    Question form added

--- mysite/views.py
+++ mysite/views.py
@@ -6,5 +6,13 @@ class MainView(View):
     def get(self, request, *args, **kwargs):
         return render(
             request,
-            'mysite/index.html'
+            'mysite/index.html',
+        )
+
+    def post(self, request, *args, **kwargs):
+        question = request.POST['QUEST']
+        print(question)
+        return render(
+            request,
+            'mysite/index.html',
         )
