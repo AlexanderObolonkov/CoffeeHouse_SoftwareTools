dafe32745c99a74412b40d9b0d800cc03bc9f40f
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-04-05 15:04:33 +0300

    Comments added

--- mysite/views.py
+++ mysite/views.py
@@ -4,18 +4,22 @@ from .forms import QuestionForm
 
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest
 from django.core.mail import send_mail, BadHeaderError
 
 
 class MainView(View):
-    def get(self, request, *args, **kwargs):
+    """View главной станицы"""
+    def get(self, request: HttpRequest, *args, **kwargs) -> render:
+        """GET запрос для главной страницы, возвращает главную страницу"""
         form = QuestionForm()
         return render(request, 'mysite/index.html', context={
             'form': form,
         })
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> render:
+        """POST запрос для главной страницы, возвращает страницу Success в случае
+        успеха, Index с ошибками в случае неуспеха"""
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.cleaned_data['question']
