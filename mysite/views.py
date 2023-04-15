c92506df4f04a13cac708fdeb44d2ad1abc062a6
aleksandr obolonkov <itbolonkov2019@gmail.com>
2023-04-15 10:27:32 +0300

    Dict for data added, trace call for MainView.post added

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,4 @@
+import pdb
 from os import getenv
 from datetime import date
 from .forms import QuestionForm
@@ -9,17 +10,17 @@ from django.core.mail import send_mail, BadHeaderError
 
 
 class MainView(View):
-    """View главной станицы"""
-    def get(self, request: HttpRequest, *args, **kwargs) -> render:
-        """GET запрос для главной страницы, возвращает главную страницу"""
+    """Class Based View главной станицы"""
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """GET запрос для главной страницы, возвращает http ответ с главной странией"""
         form = QuestionForm()
         return render(request, 'mysite/index.html', context={
             'form': form,
         })
 
-    def post(self, request: HttpRequest, *args, **kwargs) -> render:
-        """POST запрос для главной страницы, возвращает страницу Success в случае
-        успеха, Index с ошибками в случае неуспеха"""
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """POST запрос для главной страницы, валидирует форму и в зависимости от
+        результат возвращает http ответ либо с главной странией, либо со страницей успеха"""
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.cleaned_data['question']
@@ -29,6 +30,8 @@ class MainView(View):
                 send_mail(f'From {name} | {mail}', question, mail, [getenv('MAIN_EMAIL')])
             except BadHeaderError:
                 return HttpResponse('Bad header')
+            questions = {mail: question}
+            pdb.set_trace()
             return render(request, 'mysite/success.html', context={
                 'name': name,
                 'mail': mail,
